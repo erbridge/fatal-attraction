1966eac6c96816b2b9c3f291f620ccc729479f32
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2015-04-18 12:16:08 +0100

    Add a player and some planets and make them move.

--- js/game.js
+++ js/game.js
@@ -1,32 +1,75 @@
-GAME = {};
-
 (function() {
 
 'use strict';
 
-GAME.init = function() {
-    this.game = new Phaser.Game(
+var game,
+    planets,
+    currentPlanet,
+    players;
+
+window.startGame = function() {
+    game = new Phaser.Game(
         window.innerWidth, window.innerHeight,
         Phaser.AUTO,
         '',
         {
-            preload: this.preload,
-            create:  this.create,
-            update:  this.update,
+            preload: preload,
+            create:  create,
+            update:  update,
         }
     );
 };
 
-GAME.preload = function() {
+function preload() {
+    game.load.image('player', 'assets/player.png');
+    game.load.image('planet', 'assets/planet.png');
+}
 
-};
+function create() {
+    game.physics.startSystem(Phaser.Physics.P2JS);
 
-GAME.create = function() {
+    planets = game.add.group();
 
-};
+    for (var i = 0; i < 10; i++) {
+        var planet = planets.create(game.rnd.integerInRange(200, game.world.width - 200), game.rnd.integerInRange(200, game.world.height - 200), 'planet');
+        game.physics.p2.enable(planet);
 
-GAME.update = function() {
+        if (currentPlanet === undefined) {
+            currentPlanet = planet;
+        }
+    }
 
-};
+    players = game.add.group();
+
+    var player = players.create(100, 100, 'player');
+    game.physics.p2.enable(player);
+}
+
+function update() {
+    players.forEachAlive(movePlayer, this);
+    planets.forEachAlive(movePlanet, this)
+}
+
+function movePlayer(obj) {
+    move(obj, 15);
+}
+
+function movePlanet(obj) {
+    if (obj === currentPlanet) {
+        return;
+    }
+
+    move(obj, 5);
+}
+
+function move(obj, speed) {
+    accelerateToObject(obj, currentPlanet, speed);
+}
+
+function accelerateToObject(obj, target, speed, shouldRotate) {
+    var angle = Math.atan2(target.y - obj.y, target.x - obj.x);
+    obj.body.force.x = Math.cos(angle) * speed;
+    obj.body.force.y = Math.sin(angle) * speed;
+}
 
 })();
